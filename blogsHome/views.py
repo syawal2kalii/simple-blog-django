706772bec3a064e2fef039576c93c0387ca40011
syawal2kalii <syawal20162016@gmail.com>
2021-03-14 22:54:14 +0800

    1.2 mvt menggunakan render

--- blogsHome/views.py
+++ blogsHome/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 
 def blogs(request):
     print("a")
-    return HttpResponse("test")
+    # return HttpResponse("test")
+    return render(request, "index1.html")
 # Create your views here.
