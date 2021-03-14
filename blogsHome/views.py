ea0166b26f55b85b75aafa8993ec2b033eab7293
syawal2kalii <syawal20162016@gmail.com>
2021-03-15 00:11:27 +0800

    MVT membuat template menggunakan template engine

--- blogsHome/views.py
+++ blogsHome/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def blogs(request):
     print("a")
     # return HttpResponse("test")
-    return render(request, "index1.html")
+    return render(request, 'blogsHome/index.html')
 # Create your views here.
